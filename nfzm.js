da34a60907d77b58304bb49bb3d4868332c14ba4
Zeva <36792308+ZevaXu@users.noreply.github.com>
2022-03-03 22:30:26 +0800

    Update nfzm.js

--- nfzm.js
+++ nfzm.js
@@ -3,12 +3,12 @@ var obj = JSON.parse(body);
 var url = $request.url;
 
 if (/\/mobile\/user/.test(url) && obj.data){
-    obj.data.menber_type = 5;
+    obj.data.member_type = 5;
     obj.data.expire_time = "2030-05-05";
 }
 
 if (/\/mobile\/contens/.test(url) && obj.data.user){
-    obj.data.user.menber_type = 5;
+    obj.data.user.member_type = 5;
     obj.data.user.expire_time = "2030-05-05";
     obj.data.user.islogin = true;
     obj.data.user.isview = true;
