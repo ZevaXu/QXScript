43b79f085c1f36a7730838cfee1f4f13130989a8
Zeva <36792308+ZevaXu@users.noreply.github.com>
2022-03-03 20:33:45 +0800

    Add files via upload

---
+++ nfzf.js
@@ -0,0 +1,13 @@
+var body = $response.body;
+var obj = JSON.parse(body);
+
+obj.menber_type = 5;
+obj.expire_time = "2030-05-05";
+obj.islogin = true;
+obj.isview = true;
+obj.isNewsStand = 2;
+obj.isdigg = true;
+obj.isfav = true;
+
+body = JSON.stringify(obj);
+$done({body});
