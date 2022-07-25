50faa9de18f1cc217658f3764494e202d9a20c0c
zh xu <zhxu@xzh-MBA.local>
2022-07-25 15:35:26 +0800

    modify wxts.js

--- wxts.js
+++ wxts.js
@@ -3,14 +3,14 @@ var url = $request.url;
 
 
 const path1 = '/mine';
-const path2 = '/pay/memberinfo?forceRefresh';
+const path2 = '/pay/memberinfo?forceRefresh=1';
 const path3 = '/timewallet'
 
 
-if (url.indexOf(path1) != -1){
+if (url.indexOf(path1) != -1) {
     let obj = JSON.parse(body);
 
-    obj.subscriptionInfo.desc = "您已是会员";
+    obj.subscriptionInfo.desc = "已免费畅听 999 天";
     obj.subscriptionInfo.buttonText = "";
     obj.timeWalletInfo.label = "你是会员 · 拥有无限时长";
     obj.timeWalletInfo.remainSeconds = 0;
@@ -19,20 +19,25 @@ if (url.indexOf(path1) != -1){
     body = JSON.stringify(obj);
 }
 
-if (url.indexOf(path2) != -1){
+if (url.indexOf(path2) != -1) {
     let obj = JSON.parse(body);
 
     obj.isMember = 1;
     obj.startTime = 1646540603;
     obj.endTime = 1741237583;
+    obj.autoRenewableChannel = 129;
+    obj.autoRenewableTime = 1741237599;
     obj.expiresIn = 94696920;
-    obj.subscriptionDesc = "已加入会员";
+    obj.subscriptionPrice = 100;
+    obj.historyAutoRenewable = true;
+    obj.hasPromoRight = true;
+    obj.subscriptionDesc = "已加入会员999天";
     obj.subscriptionButtonLabel = "";
 
     body = JSON.stringify(obj);
 }
 
-if (url.indexOf(path3) != -1){
+if (url.indexOf(path3) != -1) {
     let obj = JSON.parse(body);
 
     obj.remainSeconds = 999999;
@@ -41,11 +46,8 @@ if (url.indexOf(path3) != -1){
     body = JSON.stringify(obj);
 }
 
-$done({body});
+$done({ body });
 
 /*
 ^https?:\/\/i\.at\.qq\.com\/(mine|pay\/memberinfo\?forceRefresh|timewallet)
-
-
 */
-
